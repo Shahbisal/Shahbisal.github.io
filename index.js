333963fd1ad879ff2265f1eae5a735ad44ff8926
Shahbisal <bisalshah66@gmail.com>
2025-12-27 17:11:19 +0530

    -

--- index.js
+++ index.js
@@ -135,7 +135,9 @@ const projectData = {
         description: "A collection of hero sketches, NPC concepts, and costume designs created in Photoshop and Procreate.",
         gameLink: "", // Empty = No 'Play' button will appear
         images: [
-            'pending design.png',
+            'CD1.png',
+            'CD2.png',
+            'CD3.png',
             
         ]
     },
